7fe7f9ea269c36a31b8cd0ece2a8dac6dc6ccbf7
siam786 <shohrab.hossain36@gmail.com>
2021-02-01 15:13:45 +0600

    rr

--- array/3.16-array-utility.php
+++ array/3.16-array-utility.php
@@ -1,2 +1,29 @@
-<?php 
-$colors = array(11,4,7,2,21,12,9);
+<?php
+$numbers = array(3, 4, 5, 7, 2, 21, 12, 9);
+
+function square($n)
+{
+    printf("Square of %d is %d \n", $n, $n * $n);
+}
+array_walk($numbers, 'square');
+
+function cube($n)
+{
+    return $n * $n * $n;
+}
+
+function even($n)
+{
+    return $n % 2 == 0;
+}
+
+function odd($n)
+{
+    return $n  % 2 == 1;
+}
+
+
+
+// $newarray = array_map('cube',$numbers);
+$newarray = array_filter($numbers, 'odd'); //calback function
+print_r($newarray);
