353444ed50922c9c904fa2c323a1b63c770b84e2
siam786 <shohrab.hossain36@gmail.com>
2021-02-01 18:50:58 +0600

    ok

--- array/3.20-array-suffle.php
+++ array/3.20-array-suffle.php
@@ -1,1 +1,13 @@
-<?php 
+<?php
+
+$numbers = range(0, 70);
+$random = mt_rand(0, 70);
+
+echo $numbers[$random] . "\n";
+
+$lucyNumber = $numbers[$random];
+if ($lucyNumber % 2 == 0) {
+    echo "head";
+} else {
+    echo "Tail";
+}
